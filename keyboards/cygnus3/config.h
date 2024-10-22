2215a40116141f1ebb8a85b5571425aefaf0e868
Ricardo Sanz <me@ricardosa.nz>
2024-10-23 01:25:04 +0200

    cygnus3: enable comms

--- keyboards/cygnus3/config.h
+++ keyboards/cygnus3/config.h
@@ -17,3 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #define EE_HANDS
+
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_TX_PIN GP29
+#define SERIAL_USART_RX_PIN GP28
